71080c525954fddbd4caba8cad42a4dc4cc1766e
xhx <xhx@dafsic.local>
2023-03-03 10:29:27 +0800

    start erc20

--- src/Counter.sol
+++ src/Counter.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.13;
 contract Counter {
     uint256 public number;
 
+    // setNumber set newNumber to number;
     function setNumber(uint256 newNumber) public {
         number = newNumber;
     }
